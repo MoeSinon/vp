5fe3ce186bf7e58fce4d8ebe1315c5903a6c78fb
MoeSinon <jojo@vanbi.tk>
2022-05-04 01:42:16 +0800

    gfh

--- havedocker/rss.sh
+++ havedocker/rss.sh
@@ -6,7 +6,7 @@ set +e
 
 cd
 mkdir -p /jk
-touch /jk/kk.sql
+touch ../jk/kk.sql
 echo "CREATE DATABASE trojan CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" >>/jk/kk.sql
 echo "CREATE DATABASE netdata CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" >>/jk/kk.sql
 echo "CREATE DATABASE roundcubemail CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" >>/jk/kk.sql
