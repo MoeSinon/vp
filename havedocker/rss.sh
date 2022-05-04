ae8076f3014d17b1ab72c9b4f5accce327b65a0d
MoeSinon <jojo@vanbi.tk>
2022-05-04 09:53:27 +0800

    1

--- havedocker/rss.sh
+++ havedocker/rss.sh
@@ -163,11 +163,11 @@ EOF
   sed -i "s/adminadmin/${password1}/g" docker-compose.yml
   docker-compose build --pull
   docker-compose up -d
-  rm -rf /jk
   # usermod -a -G redis www-data
   # mysql -u root -e "CREATE DATABASE nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
   # mysql -u root -e "create user 'nextcloud'@'localhost' IDENTIFIED BY '${password1}';"
   # mysql -u root -e "GRANT ALL PRIVILEGES ON nextcloud.* to nextcloud@'localhost';"
   # mysql -u root -e "flush privileges;"
   cd
+  rm -rf /jk
 }
